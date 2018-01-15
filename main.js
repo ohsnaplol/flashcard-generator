ca5bf6ed3c4846bdac04ef339d6a6895bb32d906
ohsnaplol <emyb@bellsouth.net>
2018-01-14 19:05:23 -0500

    testing implementations

---
+++ main.js
@@ -0,0 +1,13 @@
+var BasicCard = require('./BasicCard.js')
+var ClozeCard = require('./ClozeCard.js')
+
+var myBasicCard = new BasicCard('marcello', 'galvan')
+
+console.log(myBasicCard.front)
+console.log(myBasicCard.back)
+
+var myClozeCard = new ClozeCard('Sebastian sings the song "Under the Sea" in The Little Mermaid', 'Sebastian')
+
+console.log(myClozeCard.cloze)
+console.log(myClozeCard.fullText)
+console.log(myClozeCard.partial)
