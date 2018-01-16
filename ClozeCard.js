7e91ab1299df6858365dab4e2a0b42a4e59de046
ohsnaplol <emyb@bellsouth.net>
2018-01-16 14:44:51 -0500

    throw an error if we try to contruct invalid cloze card

--- ClozeCard.js
+++ ClozeCard.js
@@ -4,7 +4,11 @@ function ClozeCard(text, cloze) {
   if (!(this instanceof ClozeCard)) {
     return new ClozeCard(text, cloze)
   }
-  this.cloze = cloze
-  this.fullText = text
-  this.partial = this.fullText.replace(this.cloze, '...')
+  if (text.includes(cloze)) {
+    this.cloze = cloze
+    this.fullText = text
+    this.partial = this.fullText.replace(this.cloze, '...')
+  } else {
+    throw 'Text does not contain the cloze'
+  }
 }
